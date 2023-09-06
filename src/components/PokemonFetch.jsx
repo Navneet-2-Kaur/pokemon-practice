96e435ccd6f252ac3377dc78f9c7a946fab5d473
Navneet Kaur <n.kaur.sjsu@gmail.com>
2023-09-06 15:05:45 -0400

    update with axios

--- src/components/PokemonFetch.jsx
+++ src/components/PokemonFetch.jsx
@@ -1,54 +1,41 @@
 import React from "react";
 import { useState } from "react";
 import PokemonEntry from "./PokemonEntry";
-
-
-
-
+import axios from "axios";
 
 const PokemonFetch = () => {
   const [pokemonList, setPokemonList] = useState([]);
-
-  const getPokemonFetch = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
-      .then((response) => {
-        // not the actual JSON response body but the entire HTTP response
-        return response.json();
-      })
-      .then((jsonRes) => {
-        // we now run another promise to parse the HTTP response into usable JSON
-        console.log(jsonRes.results);
-        setPokemonList(jsonRes.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-      return (
-        <fieldset>
-          
-          <button  style={{backgroundColor:"grey", color:"white", fontSize:"20px"}} onClick={ getPokemonFetch } >Fetch Pokemon </button>
-
-        <div>
-          {
+  
+  const getPokemonAxios = () => {
+    axios
+    .get('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+    .then(res => {
+      // console.log(res.results);
+      setPokemonList(res.data.results);
+    })
+    .catch((err) => console.log(err));
+  }
+    
+    return (
+      <fieldset>
+      <button onClick={getPokemonAxios} >Axios Pokemon Data</button>
+      <div>
+        {
           pokemonList ? 
-            pokemonList.map( pokemon => {
-              return (
-                <PokemonEntry
-                pokeName={pokemon.name}
-            /> 
+          pokemonList.map( pokemon => {
+            return (
+              <PokemonEntry
+              pokeName={pokemon.name}
+              />
               )
             }
-          
-          ): <p>Click the button to get pokemon</p>
-        }
+            ): <></>
+            }
+      </div>
+  </fieldset>
 
-        </div>
+);
+};
 
-        </fieldset>
-      );
-      
-    };
-  
 export default PokemonFetch;
   
